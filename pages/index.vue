956ee296eaca3ba22c539dd0458db1abbf62f108
Fedorov Denis <ssboshen@yandex.ru>
2024-10-14 17:30:13 +0300

    add composable functions

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
+<script setup>
+const { sayHello } = useUtils();
+sayHello();
+</script>
+
 <template>
 	<div class="Main">
 		<h2 class="text-2xl">Hello from index.vue</h2>
-		<IconsBell class="text-green-800"/>
+		<IconsBell class="text-green-800" />
 		<img
 			src="@/assets/1.png"
 			alt="Fat cat 1"
