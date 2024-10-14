8bdcc07178ff5c2e728eb0ec80d5d76b797b7bb2
Fedorov Denis <ssboshen@yandex.ru>
2024-10-14 17:13:03 +0300

    add svg-icon from icones.js.org

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="Main">
 		<h2 class="text-2xl">Hello from index.vue</h2>
-		<img src="@/assets/1.png" alt="Fat cat 1" />
+		<IconsBell class="text-green-800"/>
+		<img
+			src="@/assets/1.png"
+			alt="Fat cat 1"
+		/>
+		<img
+			src="/2.png"
+			alt="Fat cat 2"
+		/>
 	</div>
 </template>
